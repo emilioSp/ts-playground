2c2c56fe7fb4ffa8c6bca2f826e32dcce8bdba2e
Emilio Spatola <emilio.spatola@gmail.com>
2022-11-02 23:10:40 +0100

    add another test

--- src/account.test.ts
+++ src/account.test.ts
@@ -1,5 +1,5 @@
 import t from 'tap';
-import { getAccounts } from './account'; 
+import { getAccounts, getAccount } from './account'; 
 import pgDB from './pgDB';
 
 t.teardown(() => {
@@ -9,4 +9,9 @@ t.teardown(() => {
 t.test('should return accounts in the db', async () => {
   const result = await getAccounts();
   t.same(result[0], { user_id: 1, name: 'Emilio', email: 'emilio@tiscali.it' });
+});
+
+t.test('should return account in the db with id = 1', async () => {
+  const result = await getAccount(1);
+  t.same(result, { user_id: 1, name: 'Emilio', email: 'emilio@tiscali.it' });
 });
--- src/account.ts
+++ src/account.ts
@@ -10,3 +10,8 @@ export const getAccounts = async (): Promise<Account[]> => {
   const rows = await pgDB.select().from<Account>('accounts');
   return rows;
 };
+
+export const getAccount = async (id: number): Promise<Account> => {
+  const row = await pgDB.select().from<Account>('accounts').where('user_id', '=', id);
+  return row[0];
+}
